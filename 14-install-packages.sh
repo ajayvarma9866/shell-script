d169bfec84aa88c320bd29b1f7e301d34738b1ae
ajayvarma <ajayvarma9866@gmail.com>
2024-04-20 17:13:24 +0530

    some-message

--- 14-install-packages.sh
+++ 14-install-packages.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 USERID=$(id -u)
+TIMESTAMP=$(date +%F-%H-%M-%S)
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+VALIDATE(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2...$R FAILURE $N"
+        exit 1
+    else   
+        echo -e "$2...$G SUCCESS $N"
+}
+
 if [ $USERID -ne 0 ]
 then 
     echo "please run this script with root access."
@@ -12,5 +29,12 @@ fi
 for i in $@
 do
     echo "package to install: $i"
-    dnf list installed $i
+    dnf list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i already installed...$Y SKIPPING $N"
+    else
+        dnf install $i -y &>>$LOGFILE
+        VALIDATE $? "installation of $i"
+    fi
 done
